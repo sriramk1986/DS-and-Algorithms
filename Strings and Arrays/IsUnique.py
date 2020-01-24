864c74c64056248eaf326e1bc8a42c632f28e90a
Sriram Krishnan <sriramk1986@gmail.com>
2020-01-24 14:17:47 +0530

    Second solution for unique string

--- Strings and Arrays/IsUnique.py
+++ Strings and Arrays/IsUnique.py
@@ -1,3 +1,4 @@
+# Solution 1: Use a set to store unique values. Time Complexity = O(N)
 def isUnique(string):
     uniqueChars = set()
     for char in string:
@@ -6,4 +7,17 @@ def isUnique(string):
         uniqueChars.add(char)
     return True
 
-# print(isUnique("repeat"))
+# Solution 2: Check whether input is ASCII or Unicode. If ASCII, then use below solution: Time complexity: O(N)
+def isUnique2(string):
+    # ASCII characters are from 0 -127
+    if len(string) > 128:
+        return False
+    
+    character_set = [False for _ in range(128)]
+    for char in string:
+        value = ord(char)
+        if character_set[value]:
+            return False
+        character_set[value] = True
+    
+    return True
